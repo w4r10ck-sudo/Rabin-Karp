f5ded1187f258e02c3ee22de7f7b5aa3267c05b2
Taha <muhammadtaha859@gmail.com>
2021-09-16 12:03:17 +0500

    rabin karp

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ weights = {}
 t = len(lst)
 for i in range(t):
     weights[lst[i]] = i
-print(weights)
 
 
 def search_pattern(txt, pat):
@@ -16,17 +15,18 @@ def search_pattern(txt, pat):
     for i in range(m):
         hp += weights[pat[i]] * 10 ** (m - i - 1)
         ht += weights[txt[i]] * 10 ** (m - i - 1)
-
+    ind = 0
     while len(txt) >= m:
         if ht == hp:
-            print('found')
+            print('found at {0}'.format(ind))
         ht -= weights[txt[0]] * 10 ** (m - 1)
         txt = txt[1:]
         ht *= 10
         if m - 1 < len(txt):
             ht += weights[txt[m - 1]] * 10 ** 0
+        ind += 1
 
 
-text = 'Geeks for Geeks'
-pattern = 'Geek'
+text = 'Rabin Karp Rabin Karp rabin karp'
+pattern = 'Karp'
 search_pattern(text, pattern)
